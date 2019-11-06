631e9fac95263393e2c6e267b8c44f81b3e8fd36
saera1988 <saera1988@hotmail.com>
2019-11-06 16:14:06 +0300

    Updates

---
+++ precache-manifest.3869de5cb82cae9c8ba60d1117b7d319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236f029578d76ce35b6f207ad73fe73e",
+    "url": "/Project2/index.html"
+  },
+  {
+    "revision": "bd7565055de555d65cd9",
+    "url": "/Project2/static/css/main.4687249c.chunk.css"
+  },
+  {
+    "revision": "32a6b71edfb28e46692c",
+    "url": "/Project2/static/js/2.4c8f15de.chunk.js"
+  },
+  {
+    "revision": "bd7565055de555d65cd9",
+    "url": "/Project2/static/js/main.826fbe3a.chunk.js"
+  },
+  {
+    "revision": "c4367087734ebc6d268e",
+    "url": "/Project2/static/js/runtime-main.f22c40c0.js"
+  },
+  {
+    "revision": "33df59f6c9eb1b279b957854900a8b97",
+    "url": "/Project2/static/media/woman-drinking-water_santacruzcore.33df59f6.jpg"
+  }
+]);
