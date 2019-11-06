7665ec5b2ae636de9c340bd89a4be01193d631fd
saera1988 <saera1988@hotmail.com>
2019-11-06 16:37:10 +0300

    edit deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
+
 import Navigation from "./Navigation";
 import Home from "./Home";
 import Water from "./Water";
 import Fitness from "./Fitness";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route,HashRouter } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
       // BrowserRouter to enable routing within the application
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Navigation />
         <Route exact path="/" component={Home} />
         {/* <Route path="/NetFact" component={NetFact} /> */}
         <Route path="/water" component={Water} />
         <Route path="/fitness" component={Fitness} />
     
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
